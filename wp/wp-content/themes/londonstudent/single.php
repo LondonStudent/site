d7ba75a4426ca9d064e3e1ea1fe94abb924174ee
Ross Mackay <ross@mcky.me>
2015-04-20 02:54:15 +0100

    Single post changes

--- wp/wp-content/themes/londonstudent/single.php
+++ wp/wp-content/themes/londonstudent/single.php
@@ -28,6 +28,40 @@
 	</div>
 
 	<div class="post__content">
+
+		<?php
+			$repeater = get_field('liveblog_content');
+			$order = array();
+			foreach( $repeater as $i => $row ) {
+				$order[ $i ] = $row['time'];
+			}
+			array_multisort( $order, SORT_DESC, $repeater );
+		?>
+		<ul>
+			<?php if( $repeater ): ?>
+			<?php foreach( $repeater as $i => $row ) { ?>
+				<?php $url = 's-' . $row['time']; ?>
+				<?php $time = date('g:ia', $timestamp); ?>
+				<li><a href="#<?php echo $url; ?>"><?php echo $time; ?></a></li>
+			<?php } ?>
+			<?php endif; ?>
+		</ul>
+		<?php if( $repeater ): ?>
+			<?php foreach( $repeater as $i => $row ) { ?>
+				<?php
+					$body = $row['body'];
+					$timestamp = $row['time'];
+					$time = date('g:ia', $timestamp);
+					$author = $row['author'];
+					$authorName = get_author_fullname($author['ID']);
+				?>
+				<section class="liveblog__section" id="s-<?php echo $timestamp; ?>">
+					<?php echo $time; ?> - <?php echo $authorName; ?>
+					<?php echo $body; ?>
+				</section>
+				<hr>
+			<?php } ?>
+		<?php endif; ?>
 		<?php the_content(); ?>
 	</div>
 
