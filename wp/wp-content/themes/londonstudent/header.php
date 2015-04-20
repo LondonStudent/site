c46bb2ab4cf572228ccaaabd57248c18d9b04d8b
jasmith93 <jasmith93@live.co.uk>
2015-04-20 04:51:21 +0100

    frontpage + nav

--- wp/wp-content/themes/londonstudent/header.php
+++ wp/wp-content/themes/londonstudent/header.php
@@ -11,16 +11,24 @@
 
 <body <?php body_class('site-wrapper'); ?>>
 
+    <svg display="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <symbol id="icon-menu" viewBox="0 0 1024 1024">
+            <path class="path1" d="M64 192h896v192h-896zM64 448h896v192h-896zM64 704h896v192h-896z"></path>
+        </symbol>
+    </svg>
+    
 <?php //wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 	<nav class="navbar">
 		<div class="navbar__left">
-			<a href="" id="nav-toggle">Sidebar</a>
+			<a href="" id="nav-toggle"><svg class="icon icon-twitter"><use xlink:href="#icon-menu"></use></svg></a>
 		</div>
 
-		<a href="/" class="navbar__logo">LondonStudent</a>
+		<a href="/" class="navbar__logo">    
+        <img src="http://i.imgur.com/CvS7W3g.png">
+        </a>
 
 		<ul class="navbar__right">
-				<li><a href="#">Search?</a></li>
+				<li><a href="/tips">Got a tip?</a></li>
 		</ul>
 	</nav>
 
--- wp/wp-content/themes/londonstudent/home.php
+++ wp/wp-content/themes/londonstudent/home.php
@@ -1,11 +1,127 @@
 <?php get_header(); ?>
 
-<section class="articles">
-	<ul class="article-row">
-		<li class="article-row__item">Article 1</li>
-		<li class="article-row__item">Article 2</li>
-		<li class="article-row__item">Article 3</li>
+<section class="article-row">
+	<ul class="headline">
+        <?php
+            // WP_Query arguments
+            $args = array (
+                'category_name'          => 'headline',
+            );
+
+            // The Query
+            $query = new WP_Query( $args );
+
+            // The Loop
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    ?>
+        <a href="<?php the_permalink(); ?>">
+            <h1><?php the_title(); ?></h1>
+            <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID, 'thumbnail') ); ?>
+<img src="<?php echo $url ?>">
+        </a>
+                    <?php
+                }
+            } else {
+                // no posts found
+            }
+
+            // Restore original Post Data
+            wp_reset_postdata();
+        ?>
 	</ul>
+    <hr>
 </section>
+<section class="article-row">
+    <ul class="front-left">
+                <?php
+            // WP_Query arguments
+            $args = array (
+                'category_name'          => 'front-left',
+            );
+
+            // The Query
+            $query = new WP_Query( $args );
+
+            // The Loop
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    ?>
+         <a href="<?php the_permalink(); ?>">
+            <h1><?php the_title(); ?></h1>
+            <?php the_post_thumbnail();?>
+        </a>
+                    <?php
+                }
+            } else {
+                // no posts found
+            }
+
+            // Restore original Post Data
+            wp_reset_postdata();
+        ?>
+    </ul>
+        <ul class="front-middle">
+                <?php
+            // WP_Query arguments
+            $args = array (
+                'category_name'          => 'front-middle',
+            );
+
+            // The Query
+            $query = new WP_Query( $args );
+
+            // The Loop
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    ?>
+        <a href="<?php the_permalink(); ?>">
+            <h1><?php the_title(); ?></h1>
+            <?php the_post_thumbnail();?>
+        </a>
+                    <?php
+                }
+            } else {
+                // no posts found
+            }
+
+            // Restore original Post Data
+            wp_reset_postdata();
+        ?>
+    </ul>
+        <ul class="front-right">
+                <?php
+            // WP_Query arguments
+            $args = array (
+                'category_name'          => 'front-right',
+            );
+
+            // The Query
+            $query = new WP_Query( $args );
+
+            // The Loop
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    ?>
+        <a href="<?php the_permalink(); ?>">
+            <h1><?php the_title(); ?></h1>
+            <?php the_post_thumbnail();?>
+        </a>
+                    <?php
+                }
+            } else {
+                // no posts found
+            }
+
+            // Restore original Post Data
+            wp_reset_postdata();
+        ?>
+    </ul>
+</section>
+<hr>
 
 <?php get_footer(); ?>
