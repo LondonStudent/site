06fb1a713b101b8ac36901e302b580b0e9b446e8
Ross Mackay <ross@mcky.me>
2015-04-29 20:00:01 +0100

    Added youtube embed

--- wp/wp-content/themes/londonstudent/functions/shortcodes.php
+++ wp/wp-content/themes/londonstudent/functions/shortcodes.php
@@ -11,4 +11,24 @@ function quote( $attributes, $content = null) {
 }
 add_shortcode( 'quote', 'quote' );
 
+
+function youtube( $attributes, $content = null) {
+	$url = $attributes['src'];
+	$caption = $attributes['caption'];
+	$html = '';
+	if ($url) {
+		$html .= '<figure>';
+		$html .= '	<div class="embed youtube__embed">';
+		$html .= '		<iframe class="embed__elem youtube__embed__iframe" src=' . $url . ' frameborder="0" allowfullscreen></iframe>';
+		$html .= '	</div>';
+		if ($caption) {
+			$html .= '	<figcaption class="embed__caption">' . $caption . '</figcaption>';
+		}
+		$html .= '</figure>';
+	}
+
+	return $html;
+}
+add_shortcode( 'youtube', 'youtube' );
+
 ?>
