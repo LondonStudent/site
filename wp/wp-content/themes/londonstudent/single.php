2f6255d14dac967cb7ec23684ffbd4276b817774
Ed Ive <558700@soas.ac.uk>
2015-05-22 20:45:19 +0100

    taking out comments

--- wp/wp-content/themes/londonstudent/single.php
+++ wp/wp-content/themes/londonstudent/single.php
@@ -24,61 +24,6 @@
 
 </article>
 
-<!-- <section class="author-card">
-	<img class="author-card__img" src="<?php echo $avatar ?>">
-	<div class="author-card__info">
-		<span class="author-card__name"><?php echo $fullName ?></span>
-		<p class="author-card__bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia debitis incidunt excepturi, voluptatem.</p>
-		<ul class="author-card__links">
-			<li class="author-card__link"><a class="icon-p icon-p--web" href="http://rossmackay.co">rossmackay.co</a></li>
-			<li class="author-card__link"><a class="icon-p icon-p--twitter" href="http://twitter.com/r_mcky">@r_mcky</a></li>
-		</ul>
-	</div>
-</section>
-
-<section class="related">
-	<h3 class="related__header">
-		Related
-	</h3>
-	<ul class="article-grid">
-		<li class="article-grid__item">
-			<a class="article-grid__link" href="#">
-				<img class="article-grid__img" src="http://p-hold.com/350/200" alt="">
-				<span class="article-grid__text">Article 1</span>
-			</a>
-		</li>
-		<li class="article-grid__item">
-			<a class="article-grid__link" href="#">
-				<img class="article-grid__img" src="http://p-hold.com/350/200" alt="">
-				<span class="article-grid__text">Article 2</span>
-			</a>
-		</li>
-		<li class="article-grid__item">
-			<a class="article-grid__link" href="#">
-				<img class="article-grid__img" src="http://p-hold.com/350/200" alt="">
-				<span class="article-grid__text">Article 3</span>
-			</a>
-		</li>
-	</ul>
-</section>
-
-<section class="related">
-	<h3 class="related__header">
-		Related
-	</h3>
-	<ul class="article-grid">
-		<li class="article-grid__item">
-			Article 1
-		</li>
-		<li class="article-grid__item">
-			Article 2
-		</li>
-		<li class="article-grid__item">
-			Article 3
-		</li>
-	</ul>
-</section> -->
-
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
