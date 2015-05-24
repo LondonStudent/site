213165692a8e0a7d4c6ce7cab7a587bd758e9dbe
jasmith93 <jasmith93@live.co.uk>
2015-05-24 20:26:41 +0100

    Untracked files from previous commit

---
+++ wp/wp-content/themes/londonstudent/byline.php
@@ -0,0 +1,3 @@
+<h1 class="post__title"><?php the_title(); ?></h1>
+	<div class="post__meta">
+        <p><?php the_time('F jS, Y'); ?> by <a href="<?php echo $url; ?>"><?php echo $url2 ?></a></p>
---
+++ wp/wp-content/themes/londonstudent/comment-header.php
@@ -0,0 +1,16 @@
+<div class="commentdiv">
+
+    <?php get_header(); ?>
+
+    <h1>
+
+        <?php the_title(); ?>
+
+        <br>
+    </h1>
+
+    <h2>
+        <?php the_time('F jS, Y'); ?> by <?php the_author_meta('display_name'); ?></h2>
+
+
+</div>
